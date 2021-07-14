949cdc23a1ba534fc86e57c2b38ce4508fb97384
uyayimon <mmjhmk@gmail.com>
2021-07-14 22:56:27 +0900

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class Sample extends React.Component {
+
+  state = {
+    name: "hoge",
+    age: 33
+  }
+
+  changeName = () => {
+    this.setState({
+      name: "foo"
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.sample1}>
+        <Text>こんにちは。{this.state.name}さん。</Text>
+        <Button
+          title="change Name"
+          onPress={this.changeName}></Button>
+        <Hello to="Bob"></Hello>
+        <Hello to="Tom"></Hello>
+      </View>
+    );
+  }
 }
 
+  const Hello = (props) => {
+    return (
+      <Text>Hello, {props.to}!</Text>
+    );
+  }
+
 const styles = StyleSheet.create({
-  container: {
+  sample1: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 80,
+    alignItems:"center",
   },
 });
